450149fb095e31c46c129a9c9f04cf3788fb5cf5
Mahmoud Miz <mahmoudmaiz@icloud.com>
2020-09-18 01:06:26 -0300

    fixing jwt err

--- api/middleware/user-auth.js
+++ api/middleware/user-auth.js
@@ -1,13 +1,15 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = async (req, res, next) => {
+module.exports = (req, res, next) => {
   try {
-    const token = req.headers.Authorization.split(" ")[1];
-    const decoded = await jwt.verify(token, process.env.JWT_KEY);
+    const token = req.headers.authorization.split(" ")[1];
 
-    if (decoded) {
-      next();
-    }
+    jwt.verify(token, process.env.JWT_KEY, function (err, decoded) {
+      if (err) return res.status(500).json({ message: "jwt err" });
+      if (decoded) {
+        return next();
+      }
+    });
   } catch (error) {
     return res.status(401).json({
       message: "auth failed",
