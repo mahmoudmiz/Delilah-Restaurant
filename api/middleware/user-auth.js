441c71840c7897c5a07be5f0d05ebd3f30946e99
Mahmoud Miz <mahmoudmaiz@icloud.com>
2020-09-18 15:09:59 -0300

    #user auth fix

--- api/middleware/user-auth.js
+++ api/middleware/user-auth.js
@@ -3,8 +3,8 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    console.log(token, process.env.JWT_KEY);
-    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    console.log(token);
+    const decoded = jwt.verify(token, "super");
 
     if (decoded) {
       next();
