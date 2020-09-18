84972d353395b1b959360a435ad854f8c88aba1c
Mahmoud Miz <mahmoudmaiz@icloud.com>
2020-09-17 23:57:17 -0300

    #fixing await

--- api/middleware/user-auth.js
+++ api/middleware/user-auth.js
@@ -1,13 +1,9 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   try {
-    console.log("this is the fucken jwt key" + process.env.JWT_KEY);
-    console.log(req.headers.authorization);
-    console.log(req.headers);
-    console.log(req.headers.authorization.split(" ")[1]);
     const token = req.headers.authorization.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    decoded = await jwt.verify(token, process.env.JWT_KEY);
 
     if (decoded) {
       next();
