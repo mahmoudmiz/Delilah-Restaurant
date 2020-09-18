80963c14fa85e5d57b45528a062ff190cf893193
Mahmoud Miz <mahmoudmaiz@icloud.com>
2020-09-18 01:31:41 -0300

    #fix deploy

--- api/middleware/user-auth.js
+++ api/middleware/user-auth.js
@@ -3,13 +3,11 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
 
-    jwt.verify(token, process.env.JWT_KEY, function (err, decoded) {
-      if (err) return res.status(500).json({ message: "jwt err" });
-      if (decoded) {
-        return next();
-      }
-    });
+    if (decoded) {
+      next();
+    }
   } catch (error) {
     return res.status(401).json({
       message: "auth failed",
